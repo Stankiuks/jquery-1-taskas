53cf098cb32b713c4903e74667a1489ebcc71f20
Evaldas Stankus <evalsta@gmail.com>
2017-12-21 19:21:51 +0200

    Padaryta trys burbulai

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$('document').ready(function () {
+    var pointer = 10;
+    var addedGreenValue = false;
+    var addedRedValue = false;
+
+  $('.task1 .red-ball').on('click', function () {
+      var value = parseInt ($(this).text());
+      var newValue = value * 2;
+      $(this).text(newValue);
+  });
+
+
+    $('.task2 .red-ball').on('click', function () {
+        var greenBallValue = parseInt($('.task2 .green-ball').text());
+        var value = parseInt($(this).text());
+        var newValue;
+
+        if (!addedGreenValue){
+            newValue = value + greenBallValue + pointer;
+            addedGreenValue = true;
+        }else{
+            newValue = value + pointer;
+        }
+        $(this).text(newValue);
+
+        addedRedValue = false;
+    });
+
+    $('.task2 .green-ball').on('click', function () {
+        var redBallValue = parseInt($('.task2 .red-ball').text());
+        var value = parseInt($(this).text());
+        var newValue;
+
+        if(!addedRedValue){
+            newValue = value + redBallValue + pointer;
+            addedRedValue = true;
+        }else {
+            newValue = value + pointer;
+        }
+
+        $(this).text(newValue);
+
+        addedGreenValue = false;
+    });
+});
